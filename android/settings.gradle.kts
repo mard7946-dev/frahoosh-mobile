ba574bdb4a9b94d07c425f1af0f2645d5e210282
mard7946-dev <mard7946@gmail.com>
2026-08-14 21:13:20 +0330

    Update Gradle plugins to versions 8.6.1 and 2.1.0

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,6 +4,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("com.android.application") version "8.6.1"
+        id("org.jetbrains.kotlin.android") version "2.1.0"
+    }
 }
 
 dependencyResolutionManagement {
