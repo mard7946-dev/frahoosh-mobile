a6f53c2602780e476757b6782fe0493d0fee6f01
mard7946-dev <mard7946@gmail.com>
2026-08-14 20:52:20 +0330

    Configure Android build settings in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,1 +1,39 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "ir.frahoosh.mobile"
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ir.frahoosh.mobile"
+
+        minSdk = 23
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
