391fedf3e2ee1b99c1ccba46548fae8535b11aea
mard7946-dev <mard7946@gmail.com>
2026-08-14 21:02:38 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,38 @@
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "ir.frahoosh.mobile"
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ir.frahoosh.mobile"
+        minSdk = 23
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
