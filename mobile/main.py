cbc0d3be95de7223d7ddc74d89ab2becfd552aee
mard7946-dev <mard7946@gmail.com>
2026-08-15 16:38:38 +0330

    Refactor build method in FrahooshMobileApp

--- mobile/main.py
+++ mobile/main.py
@@ -1,24 +1,22 @@
-```python
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
 from screens.login import LoginScreen
 
-
 class FrahooshMobileApp(App):
 
-    def build(self):
-        manager = ScreenManager()
-
-        manager.add_widget(
-            LoginScreen(name="login")
-        )
-
-        manager.current = "login"
-
-        return manager
-
-
-if __name__ == "__main__":
-    FrahooshMobileApp().run()
 ```
+def build(self):
+    manager = ScreenManager()
+
+    manager.add_widget(
+        LoginScreen(name="login")
+    )
+
+    manager.current = "login"
+
+    return manager
+```
+
+if **name** == "**main**":
+FrahooshMobileApp().run()
