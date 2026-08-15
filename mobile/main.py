99e2f332fcf8dfc55968d423e52158b6bcd303e1
mard7946-dev <mard7946@gmail.com>
2026-08-15 21:54:05 +0330

    Update main.py

--- mobile/main.py
+++ mobile/main.py
@@ -1,35 +1,14 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.label import Label
-from kivy.uix.screenmanager import Screen
-
-
-class TestScreen(Screen):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self.add_widget(
-            Label(
-                text="فراهوش\nآزمایش اجرای اندروید",
-                font_size=28
-            )
-        )
 
 
 class FrahooshMobileApp(App):
 
     def build(self):
-
-        manager = ScreenManager()
-
-        manager.add_widget(
-            TestScreen(name="test")
+        return Label(
+            text="FRAHOOSH TEST",
+            font_size=32
         )
-
-        manager.current = "test"
-
-        return manager
 
 
 if __name__ == "__main__":
