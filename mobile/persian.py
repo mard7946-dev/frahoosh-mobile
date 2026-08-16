b9af7004022ec8e4fde5e100c35384cf30c3002a
mard7946-dev <mard7946@gmail.com>
2026-08-16 11:01:09 +0330

    Add Persian text reshaping and display function

--- mobile/persian.py
+++ mobile/persian.py
@@ -1,1 +1,10 @@
+import arabic_reshaper
+from bidi.algorithm import get_display
 
+
+def fa(text):
+    if not text:
+        return text
+
+    reshaped_text = arabic_reshaper.reshape(str(text))
+    return get_display(reshaped_text)
