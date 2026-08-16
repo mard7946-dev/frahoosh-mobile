1daaaddab908f062229c94bcf8a229a2df658db3
mard7946-dev <mard7946@gmail.com>
2026-08-16 04:35:05 +0330

    Remove DashboardScreen from screen manager
    Removed DashboardScreen from the app's screen manager.

--- mobile/main.py
+++ mobile/main.py
@@ -2,21 +2,15 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
 from screens.login import LoginScreen
-from screens.dashboard import DashboardScreen
 
 
 class FrahooshMobileApp(App):
 
     def build(self):
-
         manager = ScreenManager()
 
         manager.add_widget(
             LoginScreen(name="login")
-        )
-
-        manager.add_widget(
-            DashboardScreen(name="dashboard")
         )
 
         manager.current = "login"
